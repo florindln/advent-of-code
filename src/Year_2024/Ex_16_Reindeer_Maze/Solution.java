55ccbd458c2ffe73b0070cb1f04609c00d56276c
florindln <deleanu.f30@gmail.com>
2024-12-16 15:09:16 +0100

    add day 16 part 2

--- src/Year_2024/Ex_16_Reindeer_Maze/Solution.java
+++ src/Year_2024/Ex_16_Reindeer_Maze/Solution.java
@@ -13,9 +13,12 @@ enum Direction{
 
 record Position(int r,int c){}
 
+record Pair<T,R>(T left,R right){}
 
 public class Solution {
-    int globalMinScore;
+    int globalMinScore=Integer.MAX_VALUE;
+
+    List<Pair<boolean[][],Integer>> allVisited=new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         File input = new File("src/Year_2024/Ex_16_Reindeer_Maze/input.txt");
@@ -35,7 +38,35 @@ public class Solution {
         Solution solution=new Solution();
 
         System.out.println("Result part 1: "+ solution.getLowestScoreThroughMaze(matrix));
-//        System.out.println("Result part 2: "+res2);
+
+        var res2Visited=new boolean[matrix.size()][matrix.get(0).length];
+        for (var pair:solution.allVisited){
+            if(pair.right()==solution.globalMinScore){
+                //matrice is one of the optimal paths, overlap the true values with our result
+                for (int i = 0; i < pair.left().length; i++) {
+                    for (int j = 0; j < pair.left()[0].length; j++) {
+                        if(pair.left()[i][j])
+                            res2Visited[i][j]=true;
+                    }
+                }
+            }
+
+        }
+        System.out.println("Result part 2: "+(getVisitedCells(res2Visited)+1));
+
+//        System.out.println(printVisited(res2Visited));
+//        solution.allVisited.forEach(a-> System.out.println(printVisited(a.left())));
+    }
+
+    public static String printVisited(boolean[][] visited){
+        var res="";
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited[0].length; j++) {
+                res+=visited[i][j]?"X":".";
+            }
+            res+="\n";
+        }
+        return res;
     }
 
     private int getLowestScoreThroughMaze(List<char[]> matrix) {
@@ -43,11 +74,10 @@ public class Solution {
             for (int c = 0; c < matrix.get(0).length; c++) {
                 if(matrix.get(r)[c]=='S'){
                     var visited=new boolean[matrix.size()][matrix.get(0).length];
-                    globalMinScore = Integer.MAX_VALUE; // Initialize global score
                     var optimalPath=new ArrayList<Direction>();
                     var res= recurseMaze(matrix,r,c,0,Direction.RIGHT,visited,new ArrayList<>(),optimalPath,new HashMap<>());
 
-                    System.out.println(optimalPath);
+//                    System.out.println("size optimal end "+optimalPath.size()+" "+optimalPath);
 
                     return res;
                 }
@@ -57,20 +87,29 @@ public class Solution {
     }
 
     private int recurseMaze(List<char[]> matrix, int r, int c, int score, Direction prevDir, boolean[][] visited, List<Direction> path, List<Direction> optimalPath, Map<String,Integer> memo) {
+//        var visString=printVisited(visited);
         if(matrix.get(r)[c]=='#' || visited[r][c]|| score>globalMinScore)
             return Integer.MAX_VALUE;
 
         if(matrix.get(r)[c]=='E'){
-            if (score < globalMinScore) {
-                globalMinScore = score;
-                optimalPath.clear();
-                optimalPath.addAll(path);
+            globalMinScore = score;
+            optimalPath.clear();
+            optimalPath.addAll(path);
+
+            //need a deep copy of visited
+            var newVisited=new boolean[visited.length][visited[0].length];
+            for (int i = 0; i < visited.length; i++) {
+                for (int j = 0; j < visited[0].length; j++) {
+                    newVisited[i][j]=visited[i][j];
+                }
             }
+            allVisited.add(new Pair<>(newVisited,score));
             return score;
         }
 
         var state=r+" "+c+" "+prevDir+" "+score;
-        if(memo.containsKey(state))
+        //additional check for max value otherwise we will memoize the optimal path and return early even though there could be multiple optimal paths
+        if(memo.containsKey(state) &&memo.get(state)==Integer.MAX_VALUE)
             return memo.get(state);
 
         visited[r][c]=true;
@@ -96,6 +135,17 @@ public class Solution {
 
         memo.put(state,res);
         return res;
+    }
+
+    private static int getVisitedCells(boolean[][] visited) {
+        var numVisited=0;
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited[0].length; j++) {
+                if(visited[i][j])
+                    numVisited++;
+            }
+        }
+        return numVisited;
     }
 
     private static Position getNewPosition(Direction dir, int r, int c) {
