fdd5c6cd5c1011ef03e94209f368bcd314cce874
florindln <deleanu.f30@gmail.com>
2024-12-20 00:03:02 +0100

    add day 17 part 2

--- src/Year_2024/Ex_17_Chronospatial_Computer/Solution.java
+++ src/Year_2024/Ex_17_Chronospatial_Computer/Solution.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 
 //record ChronoCalc(int A,int B,int C,List<Integer> instructions){}
 class ChronoComputer {
-    public int A;
-    public int B;
-    public int C;
+    public long A;
+    public long B;
+    public long C;
     public List<Integer> instructions;
-    public ChronoComputer(int A, int B, int C, List<Integer> instructions) {
+    public ChronoComputer(long A, long B, long C, List<Integer> instructions) {
         this.A = A;
         this.B = B;
         this.C = C;
@@ -29,7 +29,7 @@ class ChronoComputer {
                     var numerator = A;
                     var denominator = Math.pow(2, comboOperand);
                     var result = numerator / denominator;
-                    A = (int) result;
+                    A = (long) result;
                 }
                 case 1 -> {
                     B = B ^ literalOperand;
@@ -47,19 +47,19 @@ class ChronoComputer {
                     B = B ^ C;
                 }
                 case 5 -> {
-                    output.add(comboOperand % 8);
+                    output.add((int)(comboOperand % 8));
                 }
                 case 6 -> {
                     var numerator = A;
                     var denominator = Math.pow(2, comboOperand);
                     var result = numerator / denominator;
-                    B = (int) result;
+                    B = (long) result;
                 }
                 case 7 -> {
                     var numerator = A;
                     var denominator = Math.pow(2, comboOperand);
                     var result = numerator / denominator;
-                    C = (int) result;
+                    C = (long) result;
                 }
             }
         }
@@ -68,7 +68,7 @@ class ChronoComputer {
     int getLiteralOperand(int operand){
         return operand;
     }
-    int getComboOperand(int operand){
+    long getComboOperand(int operand){
        return switch (operand){
            case 0,1,2,3->operand;
            case 4->A;
@@ -86,9 +86,9 @@ public class Solution {
         Scanner myReader = new Scanner(input);
 
 
-        int A=0;
-        int B=0;
-        int C=0;
+        long A=0L;
+        long B=0L;
+        long C=0L;
         var instructions=new ArrayList<Integer>();
         while (myReader.hasNextLine()) {
             String line = myReader.nextLine();
@@ -97,11 +97,11 @@ public class Solution {
             var split = line.split(":");
             var beforeTwoDots = split[split.length - 2];
             if (beforeTwoDots.contains("A"))
-                A = Integer.parseInt(split[split.length - 1].trim());
+                A = Long.parseLong(split[split.length - 1].trim());
             else if (beforeTwoDots.contains("B"))
-                B = Integer.parseInt(split[split.length - 1].trim());
+                B = Long.parseLong(split[split.length - 1].trim());
             else if (beforeTwoDots.contains("C"))
-                C = Integer.parseInt(split[split.length - 1].trim());
+                C = Long.parseLong(split[split.length - 1].trim());
             else {
                 var nums = split[split.length - 1].trim();
                 var numsArr = nums.split(",");
@@ -114,14 +114,45 @@ public class Solution {
 
         var chronoComputer=new ChronoComputer(A,B,C,instructions);
 
-//        System.out.println(chronoCalc);
-
         var res1=chronoComputer.applyInstructions();
-
         System.out.println("Result part 1: "+ res1.stream().map(String::valueOf).collect(Collectors.joining(",")));
 
-//        System.out.println("Result part 2: "+(getVisitedCells(res2Visited)+1));
+//        set the current value to 0. Then for i in {0, 1, 2, 3, 4, 5, 6, 7} set the current value (register A) to `current + i`. If the first output of the program equals the last instruction of the program, then multiply the current value by 8. Now again, for each i in {0, 1, 2, 3, 4, 5, 6, 7} set the current value (register A) to `current + i`. If the first output of the program equals the second-last instruction, then multiply the current value by 8 and continue in a similar fashion, until you found the solution.
+        var validA=new ArrayList<Long>();
+        Queue<Long> possibleA=new LinkedList<>();
+        possibleA.add(0L);
+        var foundA=false;
 
+        while (!possibleA.isEmpty()&&!foundA) {
+            var currentPossibleA = possibleA.poll();
+            for (int i = 0; i < 8; i++) {
+                var newA = currentPossibleA + i;
+
+                var chronoComputer2 = new ChronoComputer(newA, B, C, instructions);
+                var res2 = chronoComputer2.applyInstructions();
+
+                if (res2.equals(instructions)){
+                    validA.add(newA);
+                }
+
+                if(res2.size()>instructions.size())
+                    break;
+
+                var matchFound=true;
+                for (int res2Index = 0; res2Index < res2.size() ; res2Index++) {
+                    if(res2.get(res2.size()-1-res2Index)!=instructions.get(instructions.size()-1-res2Index)){
+                        matchFound=false;
+                        break;
+                    }
+                }
+
+                if (matchFound) {
+                    possibleA.add(newA*8);
+                }
+            }
+        }
+
+//        System.out.println(validA);
+        System.out.println("Result pairt 2: "+Collections.min(validA));
     }
-
   }
